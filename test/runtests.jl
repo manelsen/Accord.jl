b67212387edad7512939081c367e80c271ad49cb
manelsen <emanuelmoura@gmail.com>
2026-02-11 15:09:01 +0000

    Add macro robustness test suite and CI/CD GitHub Actions workflow
    Co-authored-by: manelsen <2245073+manelsen@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using Test
 using Dates
 using Accord
-using Accord: has_flag, JSON3, BucketState, url,
-    compute_base_permissions, compute_channel_permissions
+using Accord: has_flag, JSON3, StructTypes, BucketState, url,
+    compute_base_permissions, compute_channel_permissions,
+    @discord_struct, @discord_flags, @_flags_structtypes_int,
+    CommandTree, register_command!, register_component!, register_modal!, register_autocomplete!
 
 @testset "Accord.jl" begin
     include("unit/test_snowflake.jl")
@@ -11,4 +13,5 @@ using Accord: has_flag, JSON3, BucketState, url,
     include("unit/test_permissions.jl")
     include("unit/test_ratelimiter.jl")
     include("unit/test_components.jl")
+    include("unit/test_macros.jl")
 end
