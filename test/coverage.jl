52e3996c0f43a49dd15f06ac125e262a1ce189f6
manelsen <emanuelmoura@gmail.com>
2026-02-14 17:44:59 -0300

    fix: load LocalCoverage in temporary environment in CI

--- test/coverage.jl
+++ test/coverage.jl
@@ -1,5 +1,14 @@
-using LocalCoverage
+using Pkg
+
+# Check if LocalCoverage is available, if not, install it in the global environment
+try
+    using LocalCoverage
+catch
+    @info "LocalCoverage not found. Installing..."
+    Pkg.add("LocalCoverage")
+    using LocalCoverage
+end
 
 # Generate coverage report
 # This will run the tests with coverage=true and generate an HTML report in the 'coverage' directory.
-generate_pages(Accord)
+generate_pages(joinpath(@__DIR__, ".."))
